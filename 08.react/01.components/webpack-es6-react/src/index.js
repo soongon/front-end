860ad892f06a2901fd2145f29ed0ba91737a5fd7
soongon <soongon@gmail.com>
2018-04-09 14:49:03 +0900

    react

--- 08.react/01.components/webpack-es6-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './style.css';
-
-const App = () => <h2>Hello, webpack!</h2>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
