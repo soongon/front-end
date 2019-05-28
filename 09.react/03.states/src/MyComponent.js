d45ab5df71a25db2d7368d8b7f73655dae5f6749
soongon <soongon@gmail.com>
2019-05-29 00:06:50 +0900

    initial commit

--- 09.react/03.states/src/MyComponent.js
+++ 09.react/03.states/src/MyComponent.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 
 export default class MyComponent extends Component {
 
-    constructor(props) {
-        super(props);
-        this.decrease = this.decrease.bind(this);
-    }
-
     state = {
         number: 5
     };
@@ -18,11 +13,6 @@ export default class MyComponent extends Component {
     //     }
     // }
 
-    decrease() {
-        this.setState({
-            number: this.state.number - 1
-        })
-    }
 
     render() {
         return (
@@ -30,12 +20,7 @@ export default class MyComponent extends Component {
                 <p>이름 : {this.props.name}</p>
                 <p>나이 : {this.props.age}</p>
                 <p>숫자 : {this.state.number}</p>
-                <button onClick={() => {
-                    this.setState({
-                        number: this.state.number + 1
-                    })
-                }}>더하기</button>
-                <button onClick={this.decrease}>빼기</button>
+
             </div>
         );
     }
